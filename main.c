80a25b51721e5cf2e98ac937f68676773276e836
gurcei <gurce.isikyildiz@gmail.com>
2018-04-21 22:26:43 +1000

    enforce app to be uppercase

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 
 void main(void)
 {
+  // switch back to upper-case
+  // https://www.cc65.org/mailarchive/2004-09/4446.html
+  *(unsigned char *)0xd018 = 0x15;
+
   while (1)
   {
     printf("hello world!\n");
